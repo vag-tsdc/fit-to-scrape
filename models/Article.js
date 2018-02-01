77db97add1179f8e605e956738cc59848f3c3297
vag-tsdc <vagtsdc@gmail.com>
2018-01-31 22:59:44 -0500

    article.js build up

--- models/Article.js
+++ models/Article.js
@@ -1,11 +1,38 @@
 // Article.js
 
 // require mongoose
+var mongoose = require("mongoose");
+var Note = require("./Note");
 
 // create schema class
+var Schema = mongoose.Schema;
 
 // create aricle schema
+var ArticleSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    summary: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    saved: {
+      type: Boolean,
+      default: false
+    },
+    notes: [{
+       type: Schema.Types.ObjectId,
+       ref: "Note"
+    }]
+  });
 
 // create the article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
 
 // Export the model
+module.exports = Article;
