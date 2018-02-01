b9fa282e4e82a2961ba2380f68c784c5bb8f101a
vag-tsdc <vagtsdc@gmail.com>
2018-02-01 00:16:08 -0500

    we got articles for horoscopes being generated, ugly, mongo only working locally

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var app = express();
 // Use morgan and body parser
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 // Make public a static dir
@@ -47,14 +47,84 @@ mongoose.connect("mongodb://localhost/mongoscraper");
 var db = mongoose.connection;
 
 // Show any mongoose errors
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
+db.on("error", function (error) {
+    console.log("Mongoose Error: ", error);
 });
 
 // Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
+db.once("open", function () {
+    console.log("Mongoose connection successful.");
 });
 
 
 // Routes
+//GET requests to render Handlebars pages
+app.get("/", function (req, res) {
+    Article.find({
+        "saved": false
+    }, function (error, data) {
+        var hbsObject = {
+            article: data
+        };
+        console.log(hbsObject);
+        res.render("home", hbsObject);
+    });
+});
+
+app.get("/saved", function (req, res) {
+    Article.find({
+        "saved": true
+    }).populate("notes").exec(function (error, articles) {
+        var hbsObject = {
+            article: articles
+        };
+        res.render("saved", hbsObject);
+    });
+});
+
+// A GET request to scrape the echojs website
+app.get("/scrape", function (req, res) {
+    // First, we grab the body of the html with request
+    request("https://www.astrology.com/horoscopes.html", function (error, response, html) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("div.daily-horoscope").each(function (i, element) {
+
+            // Save an empty result object
+            var result = {};
+
+            // Add the title and summary of every link, and save them as properties of the result object
+            result.title = $(this).children("h1").text();
+            result.summary = $(this).children("p").text();
+            result.link = $(this).children("a").attr("href");
+            // result.link = $(this).children("a").children("h6").attr("href");
+
+            // Using our Article model, create a new entry
+            // This effectively passes the result object to the entry (and the title and link)
+            var entry = new Article(result);
+
+            // Now, save that entry to the db
+            entry.save(function (err, doc) {
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                }
+                // Or log the doc
+                else {
+                    console.log(doc);
+                }
+            });
+
+        });
+        res.send("Scrape Complete");
+
+    });
+    // Tell the browser that we finished scraping the text
+});
+
+// Listen on port
+app.listen(port, function () {
+    console.log("App running on port " + port);
+});
+
