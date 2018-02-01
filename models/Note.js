14ba071013ff24cb9cbb4b66568a9c03af890dcb
vag-tsdc <vagtsdc@gmail.com>
2018-01-31 23:02:12 -0500

    build notes.js

--- models/Note.js
+++ models/Note.js
@@ -1,12 +1,25 @@
 // Note.js
 
+
 // require mongoose
+var mongoose = require("mongoose");
 
 //  create schema class
+var Schema = mongoose.Schema;
 
 // create the Note schema
+var NoteSchema = new Schema({
+    body: {
+        type: String
+    },
+    article: {
+        type: Schema.Types.ObjectId,
+        ref: "Article"
+    }
+});
 
 //  create the Note model with the NoteSchema
+var Note = mongoose.model("Note", NoteSchema);
 
 //  Export the Note model
-
+module.exports = Note;
