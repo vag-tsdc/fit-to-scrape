ead61244ec5f66d678b4fd41909833f83d4e0639
vag-tsdc <vagtsdc@gmail.com>
2018-01-31 19:35:37 -0500

    more structure added

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 // server.js
 
-express
-express-handlebars
-mongoose
-body-parser
-cheerio
-request
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var path = require("path");
+
+// Requiring Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+// Scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+//Define port
+var port = process.env.PORT || 3000
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser
+
+// Set Handle Bars
+
+// Database Config with Mongoose
+
+// 
+
+// mongoose errors
+
+// Log Sucess Message once logged into mongoose db
+
+// Routes
