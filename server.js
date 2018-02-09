e06de290bd7695eb4d7c2b84608495043e1bc974
vag-tsdc <vagtsdc@gmail.com>
2018-02-09 02:37:59 -0500

    i was working on master, oops, more work to app build

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var cheerio = require("cheerio");
 mongoose.Promise = Promise;
 
 //Define port
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3030
 
 // Initialize Express
 var app = express();
@@ -43,7 +43,8 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/mongoscraper");
+// mongoose.connect("mongodb://localhost/mongoscraper");
+mongoose.connect("mongodb://heroku_lm01b396:lsp58euhcbs934dm3500qej9vb@ds121268.mlab.com:21268/heroku_lm01b396");
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -53,7 +54,7 @@ db.on("error", function (error) {
 
 // Once logged in to the db through mongoose, log a success message
 db.once("open", function () {
-    console.log("Mongoose connection successful.");
+    console.log("Mongoose is connected!");
 });
 
 
@@ -82,7 +83,7 @@ app.get("/saved", function (req, res) {
     });
 });
 
-// A GET request to scrape the echojs website
+// A GET request to scrape the daily horoscope site 1
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with request
     request("https://www.astrology.com/horoscopes.html", function (error, response, html) {
@@ -92,17 +93,17 @@ app.get("/scrape", function (req, res) {
         $("div.daily-horoscope").each(function (i, element) {
 
             // Save an empty result object
-            var result = {};
+            var result1 = {};
 
             // Add the title and summary of every link, and save them as properties of the result object
-            result.title = $(this).children("h1").text();
-            result.summary = $(this).children("p").text();
-            result.link = $(this).children("a").attr("href");
+            result1.title = $(this).children("h1").text();
+            result1.summary = $(this).children("p").text();
+            result1.link = $(this).children("a").attr("href");
             // result.link = $(this).children("a").children("h6").attr("href");
 
             // Using our Article model, create a new entry
             // This effectively passes the result object to the entry (and the title and link)
-            var entry = new Article(result);
+            var entry = new Article(result1);
 
             // Now, save that entry to the db
             entry.save(function (err, doc) {
@@ -123,7 +124,135 @@ app.get("/scrape", function (req, res) {
     // Tell the browser that we finished scraping the text
 });
 
+// A GET request to scrape the daily horoscope site 1: New York Post
+app.get("/scrape", function (req, res) {
+    // First, we grab the body of the html with request
+    request("https://nypost.com/horoscopes/", function (error, response, html) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article.box horoscope").each(function (i, element) {
 
+            // Save an empty result object
+            var result2 = {};
+
+            // Add the title and summary of every link, and save them as properties of the result object
+            result2.title = $(this).children("h3").text();
+            result2.summary = $(this).children(".entry-content").text();
+            result2.link = $(this).children("h3").children("a").attr("href");
+            // result.link = $(this).children("a").children("h6").attr("href");
+
+            // Using our Article model, create a new entry
+            // This effectively passes the result object to the entry (and the title and link)
+            var entry = new Article(result2);
+
+            // Now, save that entry to the db
+            entry.save(function (err, doc) {
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                }
+                // Or log the doc
+                else {
+                    console.log(doc);
+                }
+            });
+
+        });
+        res.send("Scrape Complete");
+
+    });
+    // Tell the browser that we finished scraping the text
+});
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+    // Grab every doc in the Articles array
+    Article.find({}, function(error, doc) {
+      // Log any errors
+      if (error) {
+        console.log(error);
+      }
+      // Or send the doc to the browser as a json object
+      else {
+        res.json(doc);
+      }
+    });
+  });
+  
+  // Grab an article by it's ObjectId
+  app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    Article.findOne({ "_id": req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    // now, execute our query
+    .exec(function(error, doc) {
+      // Log any errors
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise, send the doc to the browser as a json object
+      else {
+        res.json(doc);
+      }
+    });
+  });
+  
+  
+  // Save an article
+  app.post("/articles/save/:id", function(req, res) {
+        // Use the article id to find and update its saved boolean
+        Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true})
+        // Execute the above query
+        .exec(function(err, doc) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          }
+          else {
+            // Or send the document to the browser
+            res.send(doc);
+          }
+        });
+  });
+
+  // Delete an article
+
+  // Create a new note
+  app.post("/notes/save/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    var newNote = new Note({
+      body: req.body.text,
+      article: req.params.id
+    });
+    console.log(req.body)
+    // And save the new note the db
+    newNote.save(function(error, note) {
+      // Log any errors
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise
+      else {
+        // Use the article id to find and update it's notes
+        Article.findOneAndUpdate({ "_id": req.params.id }, {$push: { "notes": note } })
+        // Execute the above query
+        .exec(function(err) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+            res.send(err);
+          }
+          else {
+            // Or send the note to the browser
+            res.send(note);
+          }
+        });
+      }
+    });
+  });
+  // Delete a note
 
 
 // Listen on port
